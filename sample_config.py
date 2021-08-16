4cdcf08b2d9cdf0490193d3a889b3df8a37ee233
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-08-17 06:16:10 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -19,12 +19,12 @@ if HEROKU:
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
-    API_ID = 14371
-    API_HASH = "e46b6c854d2bf58a0"
-    ARQ_API_KEY = "Get this from @ARQRobot"
-    CHAT_ID = -100546355432
-    DEFAULT_SERVICE = "saavn"  # Must be one of "youtube"/"saavn"
+    API_ID = 2316698
+    API_HASH = "5178f2c2ce9a7d55e6bd9df407dcc71e"
+    ARQ_API_KEY = "UULQAB-YNDQXF-KPXGBY-IIKNGP-ARQ"
+    CHAT_ID = -1001478147133
+    DEFAULT_SERVICE = "youtube"  # Must be one of "youtube"/"saavn"
     BITRATE = 512 # Must be 512/320
-
+    SESSION_STRING = "BQC2x_z0dh21OdGjLp9nxjCjYuzhzszqcwcqKgdfsDgl53slPnDh-F3Ze4tPCrWf3B9FVBtjaw5c489vgT0Uinpki4uwA0u7k0yDURWlO0b_3jT5bouaFS8LSPB44UVox1voyguaqoV-DigYdkA1gr-IWz8kWT4PAcElFNyPlSNPXKiBZfk0_5uEgz5OICxNUpR3DnBBphtKxw8nPgLo5XpgJD5LAoEiWPNMeG4HX_ZOG4R8NSsMHpBo4aHN8cEk2AHISU3WVeA9zEB5ta7JEdBoQ_7BT3qrZEm5DdvNHhH7-zFCsENOJnpq6udPEpTCJ38tsBfGXF0h2GIyZn9LnoSQVbAYbwA"
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
